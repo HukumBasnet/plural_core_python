639f6961b73077898213e87026764084cd789b14
reenahukum <big.b.it.traininig@gmail.com>
2020-07-09 01:29:45 +0545

    first commit

--- intropection/core-python-introspection/m03/clip-02/mixed-2/numerals.py
+++
@@ -1,8 +0,0 @@
-from fractions import Fraction
-
-
-def mixed_numeral(vulgar):
-    integer = vulgar.numerator // vulgar.denominator
-    fraction = Fraction(vulgar.numerator - integer * vulgar.denominator,
-                        vulgar.denominator)
-    return integer, fraction
